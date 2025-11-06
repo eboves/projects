68b0673dd6b0ea7b1681f2c88f1f6820bcd4137e
Elvis <elvisboves@Elviss-MacBook-Pro.local>
2025-11-05 21:23:28 -0500

    fetching individual data

--- stock reader/main.py
+++ stock reader/main.py
@@ -6,39 +6,68 @@ import customtkinter as ctk
 
 
 
-def button_callback():
-    # print(f"You entered: {entry.get()}")
-    data = yf.download(entry.get(),start='2020-01-01', end="2025-01-01") #Gets the ticket data from Jan 01, 2020 to Jan 01, 2025
-    data["MA20"] = data["Close"].rolling(window=20).mean() #This creates a new column named MA20 holding the mean value from the previous 20 days
-    data["MA50"] = data["Close"].rolling(window=50).mean() #This creates a new column named MA50 holding the mean value from the previous 50 days
+# def button_callback():
+#     # print(f"You entered: {entry.get()}")
+#     data = yf.download(entry.get(),start='2020-01-01', end="2025-01-01") #Gets the ticket data from Jan 01, 2020 to Jan 01, 2025
+#     data["MA20"] = data["Close"].rolling(window=20).mean() #This creates a new column named MA20 holding the mean value from the previous 20 days
+#     data["MA50"] = data["Close"].rolling(window=50).mean() #This creates a new column named MA50 holding the mean value from the previous 50 days
 
-    data["signal"] = data["MA20"] > data["MA50"] #Creates a column named SIGNAL that hold the boolean values when MA20 is greater (Above) than MA50
-    data['cross_over'] = data['signal'].ne(data['signal'].shift())
+#     data["signal"] = data["MA20"] > data["MA50"] #Creates a column named SIGNAL that hold the boolean values when MA20 is greater (Above) than MA50
+#     data['cross_over'] = data['signal'].ne(data['signal'].shift())
 
-    bullish = data[(data["cross_over"]) & (data['signal'] == True)] # MA20 crosses Above the MA50
-    bearish = data[(data["cross_over"]) & (data['signal'] == False)] # MA20 crosses Bellow the MA50
+#     bullish = data[(data["cross_over"]) & (data['signal'] == True)] # MA20 crosses Above the MA50
+#     bearish = data[(data["cross_over"]) & (data['signal'] == False)] # MA20 crosses Bellow the MA50
 
-    plt.figure(figsize=(12,6))
-    sns.lineplot(data=data, x=data.index, y=data["MA20"], label='20-Day MA')
-    sns.lineplot(data=data, x=data.index, y=data["MA50"], label='50-Day MA')
-    plt.scatter(bullish.index, bullish["MA20"], color='green', label='Bullish Cross-Over', s=80, zorder=5)
-    plt.scatter(bearish.index, bearish["MA20"], color='red', label='Bearish Cross-Over', s=80, zorder=5)
-    plt.title(f'{entry.get()} Stock Price & Moving Average with Cross-Over')
-    plt.xlabel('Date')
-    plt.ylabel('Price ($)')
-    plt.legend()
-    plt.grid(True, linestyle='--', alpha=0.3)
-    plt.tight_layout()
-    plt.show()
+#     plt.figure(figsize=(12,6))
+#     sns.lineplot(data=data, x=data.index, y=data["MA20"], label='20-Day MA')
+#     sns.lineplot(data=data, x=data.index, y=data["MA50"], label='50-Day MA')
+#     plt.scatter(bullish.index, bullish["MA20"], color='green', label='Bullish Cross-Over', s=80, zorder=5)
+#     plt.scatter(bearish.index, bearish["MA20"], color='red', label='Bearish Cross-Over', s=80, zorder=5)
+#     plt.title(f'{entry.get()} Stock Price & Moving Average with Cross-Over')
+#     plt.xlabel('Date')
+#     plt.ylabel('Price ($)')
+#     plt.legend()
+#     plt.grid(True, linestyle='--', alpha=0.3)
+#     plt.tight_layout()
+#     plt.show()
 
-app = ctk.CTk()
-app.title("My App")
-app.geometry("400x150")
+# app = ctk.CTk()
+# app.title("My App")
+# app.geometry("400x150")
 
-entry = ctk.CTkEntry(master=app, placeholder_text="Enter Ticket: TSLA")
-entry.pack(pady=10)
+# entry = ctk.CTkEntry(master=app, placeholder_text="Enter Ticket: TSLA")
+# entry.pack(pady=10)
 
-button = ctk.CTkButton(app, text="my button", command=button_callback)
-button.pack(pady=5)
+# # button = ctk.CTkButton(app, text="my button", command=button_callback)
+# # button.pack(pady=5)
 
-app.mainloop()
+# app.mainloop()
+
+
+data = yf.Ticker("AAPL")
+ticket_info = data.info
+industry = ticket_info['industry']
+sector = ticket_info['sector']
+ceo_director_info = ticket_info['companyOfficers'][0]
+ceo_director = ceo_director_info['name']
+title = ceo_director_info['title']
+total_revenue = ticket_info["totalRevenue"]
+dividend_yield = ticket_info["dividendYield"]
+free_cash_flow = ticket_info["freeCashflow"]
+total_cash = ticket_info[""]
+print(dividend_yield)
+# TODO: get info from the CEO/President of the company and see in how many company it worked
+# print(ceo_director)
+# print(title)
+# Total Revenue (💵): 416161005568
+# Net Income (Profit 💰): 112010002432
+# Revenue Growth (📈): 0.079
+# Earnings Growth (📊): 0.912
+# Free Cash Flow (💵): 78862254080
+# Debt to Equity (⚖️): 152.411
+# Return on Equity (💡): 1.714
+# Profit Margin (🏦): 0.269
+# PE Ratio (💵/💼): 36.24
+# Dividend Yield (💸): 0.0039
+# Total Cash (🏦): 54697000960
+
